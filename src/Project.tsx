13d2205c6d02acadbd5ba0d5e03e63ee5d32a3ac
Freeman Bird <freemanbird@gmail.com>
2022-07-23 15:49:55 -0600

    changes to the homepage columns

--- src/Project.tsx
+++ src/Project.tsx
@@ -1,11 +1,56 @@
 import React from 'react';
+import { makeStyles } from "@material-ui/styles";
 
-const Project = () => {
+
+const useStyles = makeStyles(() => ({
+  projectWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '200px',
+    border: '1px solid black',
+    oveflow: 'scroll',
+    justifyContent: 'center'
+  },
+  projectTitle: {
+    flex: '1',
+    border: '5px solid blue',
+  },
+  imgWrapper: {
+   flex: '2',
+  border: '1px solid grey',
+
+  },
+  projectImg: {
+   width: '100px',
+   height: '50px',
+  //  flex: '1',
+   border: '1px solid red',
+  },
+
+}))
+
+
+interface IProps {
+  title: string;
+  image: string;
+  url: string;
+}
+
+
+
+const Project = ({title, image, url}: IProps) => {
+  const classes = useStyles()
+
   // onclick, go to the project
+
   return (
-    <div>
-      <p>INSERT HEADER HERE</p>
-      <img src='PROJECT_IMAGE' alt='PROJECT_IMAGE' />
+    <div className={classes.projectWrapper}>
+      <p className={classes.projectTitle}>{title}</p>
+      <div className={classes.imgWrapper}>
+        <a href={url}>
+          <img className={classes.projectImg} src={image} alt={`PROJECT_IMAGE_${image}`} />
+        </a>
+      </div>
     </div>
   )
 }
--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
+import Project from './Project';
 
 const useStyles = makeStyles(() => ({
-  projectWrapper: {
+  projectsWrapper: {
     display: 'flex',
     backgroundColor: 'purple',
     height: '70vh',
@@ -11,29 +12,134 @@ const useStyles = makeStyles(() => ({
     fontSize: '20px',
   },
   projectColumn: {
+    display: 'flex',
     flex: '1',
+    border: '1px solid yellow',
+    flexFlow: 'column',
+    overflowY: 'scroll',
   },
 }))
+
+const coleProjects = [
+  {
+    title: 'testProject',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Seleuco_I_2.JPG',
+    url: 'test'
+  },
+  {
+    title: 'testProject',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Seleuco_I_2.JPG',
+    url: 'test'
+  },
+  {
+    title: 'testProject',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Seleuco_I_2.JPG',
+    url: 'test'
+  },
+  // {
+  //   title: 'testProject',
+  //   image: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Seleuco_I_2.JPG',
+  //   url: 'test'
+  // }
+]
+
+const collabProjects = [
+{
+  title: 'testProject',
+  image: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Seleuco_I_2.JPG',
+  url: 'test'
+},
+{
+  title: 'testProject',
+  image: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Seleuco_I_2.JPG',
+  url: 'test'
+},
+{
+  title: 'testProject',
+  image: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Seleuco_I_2.JPG',
+  url: 'test'
+},
+{
+  title: 'testProject',
+  image: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Seleuco_I_2.JPG',
+  url: 'test'
+},
+{
+  title: 'testProject',
+  image: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Seleuco_I_2.JPG',
+  url: 'test'
+},
+{
+  title: 'testProject',
+  image: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Seleuco_I_2.JPG',
+  url: 'test'
+},
+{
+  title: 'testProject',
+  image: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Seleuco_I_2.JPG',
+  url: 'test'
+},
+{
+  title: 'testProject',
+  image: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Seleuco_I_2.JPG',
+  url: 'test'
+},
+]
+
+const freemanProjects = [
+  {
+    title: 'testProject',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Seleuco_I_2.JPG',
+    url: 'test'
+  },
+  {
+    title: 'testProject',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Seleuco_I_2.JPG',
+    url: 'test'
+  },
+  {
+    title: 'testProject',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Seleuco_I_2.JPG',
+    url: 'test'
+  },
+  {
+    title: 'testProject',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Seleuco_I_2.JPG',
+    url: 'test'
+  }
+]
 
 const Projects = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.projectWrapper}>
+    <div className={classes.projectsWrapper}>
       <div className={classes.projectColumn}>
         <p className={classes.projectHeader}>
           Cole's Projects
         </p>
+        {coleProjects.map((project) => {
+          return (
+             <Project title={project.title} image={project.image} url= {project.url}/>
+          )
+        })}
       </div>
       <div className={classes.projectColumn}>
         <p className={classes.projectHeader}>
           Collaborative Projects
         </p>
+        {collabProjects.map((project) => {
+          return <Project title={project.title} image={project.image} url={project.url} />
+        })}
       </div>
       <div className={classes.projectColumn}>
         <p className={classes.projectHeader}>
           Freeman's Projects
         </p>
+        {freemanProjects.map((project) => {
+          return <Project title={project.title} image={project.image} url={project.url} />
+        })}
+
       </div>
     </div>
   )
